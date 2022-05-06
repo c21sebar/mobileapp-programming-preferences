9e88d6165a01c58388bbd5d70e1208396668aa1e
Sebastian Arneson <c21sebar@student.his.se>
2022-05-06 12:37:29 +0200

    Lade till shared preferences, sparar text i edit text

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     Button nextActivity;
+    Button save;
+    TextView text;
+    EditText editText;
+    String textToNext;
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
 
     @Override
@@ -19,17 +28,36 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         nextActivity = findViewById(R.id.button2);
+        save = findViewById(R.id.button);
+        text = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+        String read = myPreferenceRef.getString("Reference","Saknar Data");
+        text.setText(read);
+
         nextActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             toSecondActivity();
             }
         });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            text.setText(editText.getText());
+            textToNext = editText.getText().toString();
+            myPreferenceEditor.putString("Reference", text.getText().toString());
+            myPreferenceEditor.apply();
+
+            }
+        });
     }
 
     void toSecondActivity(){
         Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("extra", "value");
+        intent.putExtra("extra", "" + textToNext);
         startActivity(intent);
 
     }
