d567a587c53db2fa2b803fa3f4178391fa1c309a
Sebastian Arneson <c21sebar@student.his.se>
2022-05-06 12:17:07 +0200

    Lade till activity i AndriodManifest Lade till kod som gör att second activity använder activity_second.xml Lade till knapp som gör att man kan gå tillbaka till main.

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,4 +1,34 @@
 package com.example.project;
 
-public class SecondActivity {
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    Button mySecondButton;
+    TextView secondTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mySecondButton = findViewById(R.id.secondActivityButton);
+        secondTextView = findViewById(R.id.secondActivityText);
+        final String name = getIntent().getExtras().getString("name");
+        secondTextView.setText(name);
+        mySecondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goBackToMain();
+            }
+        });
+    }
+    void goBackToMain(){
+        finish();
+    }
 }
